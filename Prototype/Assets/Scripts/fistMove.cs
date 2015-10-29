31c0b680c768fd8b0386b6f0ed2480b7a7d16cda
PFranklinM <paulmallon@Pauls-MBP.wireless.newschool.edu>
2015-10-29 13:00:24 -0400

    The Files

---
+++ Prototype/Assets/Scripts/fistMove.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fistMove : MonoBehaviour {
+
+	public GameObject fist;
+
+	private int timer = 0;
+
+	public float moveAmount = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 moving = new Vector3 (transform.position.x,
+		                              transform.position.y,
+		                              transform.position.z);
+
+
+		if (timer < 10) {
+			timer++;
+			moving.x += moveAmount * Time.deltaTime;
+		} else if (timer >= 10) {
+			Destroy(fist);
+		}
+
+		if (Input.GetButtonUp ("PS_Square")) {
+			timer = 0;
+		}
+
+		transform.position = moving;
+	
+	}
+}
---
+++ Prototype/Assets/Scripts/playerMove.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMove : MonoBehaviour {
+
+	public float moveAmount = 10f;
+
+	public float jumpSpeed = 10f;
+
+	public GameObject player;
+	public GameObject floor;
+	public GameObject fist;
+
+	//ground teteer totter - pivot point in the middle (push it up from bottom with infinite jumps)
+	//not button mashing
+	//environmental hazard based on what the background is
+
+	private int jumpCounter = 0;
+
+	Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+
+		rb = GetComponent<Rigidbody2D>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 moving = new Vector3 (transform.position.x,
+		                              transform.position.y,
+		                              transform.position.z);
+
+		if (Input.GetAxis ("DPad_Horizontal") > 0) {
+			moving.x += moveAmount * Time.deltaTime;
+		}
+
+//		if(Input.GetAxis ("Horizontal") > 0) {
+//			moving.x += moveAmount * Time.deltaTime;
+//		}
+
+		if (Input.GetAxis ("DPad_Horizontal") < 0) {
+			moving.x -= moveAmount * Time.deltaTime;
+		}
+
+//		if(Input.GetAxis ("Horizontal") < 0) {
+//			moving.x -= moveAmount * Time.deltaTime;
+//		}
+
+		if(Input.GetButtonDown ("PS_X")){
+			jumpCounter ++;
+		}
+
+		if (Input.GetButtonDown ("PS_X") && jumpCounter < 3){
+			rb.velocity = new Vector3(0, 10, 0);
+//			print (jumpCounter);
+		}
+
+		if (Input.GetButtonDown ("PS_Square")) {
+			GameObject fist = Instantiate (Resources.Load ("Fist")) as GameObject;
+			fist.transform.position = player.transform.position;
+		}
+	
+	transform.position = moving;
+
+		if (Input.GetKey (KeyCode.Return)) {
+			Application.LoadLevel("Main");
+		}
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D coll){
+		if (coll.gameObject.tag == "ground") {
+			jumpCounter = 0;
+		}
+	}
+}
+	
